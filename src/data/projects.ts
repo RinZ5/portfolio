e15025d2b368388ed88d795b3eb63d3f01175a6e
RinZ <supawit.promma6256@gmail.com>
2026-01-17 00:49:49 +0700

    feat(projects): add all projects page

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,23 +1,37 @@
-export const featuredProjects = [
+export interface Project {
+  title: string;
+  desc: string;
+  link: string;
+  image: string;
+  tags: string[];
+  isFeatured: boolean;
+}
+
+export const projects: Project[] = [
   {
     title: "Indie App 1",
     desc: "A mobile app for tracking x built with React Native.",
     link: "/projects/app-1",
     image: "/images/project-preview-1.png",
-    tags: ["React Native", "Go"]
+    tags: ["React Native", "Go"],
+    isFeatured: true,
   },
   {
     title: "SaaS Dashboard",
     desc: "Financial analytics tool using Python and Pandas.",
     link: "/projects/saas",
     image: "/images/project-preview-1.png",
-    tags: ["Python", "Vue.js"]
+    tags: ["Python", "Vue.js"],
+    isFeatured: true,
   },
   {
-    title: "Test Dashboard",
-    desc: "Financial analytics tool using Python and Pandas.",
-    link: "/projects/saas",
-    image: "/images/project-preview-1.png",
-    tags: ["Python", "Vue.js"]
+    title: "Old Experiment",
+    desc: "A simple script I wrote years ago.",
+    link: "/projects/script",
+    image: "/images/old.png",
+    tags: ["Bash"],
+    isFeatured: false,
   },
 ];
+
+export const getFeaturedProjects = () => projects.filter(project => project.isFeatured);
